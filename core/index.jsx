3dbe75f16946a954150d2e15ae227d592d9e6c0f
ozgrozer <ozgr@live.com>
2017-10-27 19:50:08 +0300

    Textarea converted to the div from textarea.

--- core/index.jsx
+++ core/index.jsx
@@ -1,28 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import te from 'trevor-engine'
+import trevorEngine from 'trevor-engine'
+
+console.error = (function (_error) {
+  return function (message) {
+    if (typeof message !== 'string' || message.indexOf('component is `contentEditable`') === -1) {
+      _error.apply(console, arguments)
+    }
+  }
+})(console.error)
 
 class App extends React.Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
     const input = '1 km to mi'
     const output = this.convert(input)
     this.state = { input: input, output: output }
+    this.handleInput = this.handleInput.bind(this)
   }
+
   convert (lines) {
-    return lines.split('\n').map(line => te(line) + '\n')
+    return lines.split('\n').map(line => trevorEngine(line) + '\n')
   }
+
   handleInput (event) {
-    const input = event.target.value
+    const input = event.target.innerText
     const output = this.convert(input)
-    this.setState({ input: input, output: output })
+    this.setState({ output: output })
   }
+
   render () {
     return (
       <div>
         <div id='drag'>Trevor</div>
-        <textarea id='input' onChange={this.handleInput.bind(this)} value={this.state.input} autoFocus />
-        <pre id='output'>{this.state.output}</pre>
+        <div id='scroll'>
+          <div id='input' contentEditable onInput={this.handleInput}>
+            <div>{this.state.input}</div>
+          </div>
+          <pre id='output'>{this.state.output}</pre>
+        </div>
       </div>
     )
   }
