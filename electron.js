5a94bc76073f878a461cf9885b0fa77fbe4b9965
ozgrozer <ozgr@live.com>
2017-10-29 15:07:11 +0300

    Trevor now remembers the last coordinates and width, height sizes of the window on launch.

--- electron.js
+++ electron.js
@@ -1,26 +1,48 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const Store = require('electron-store-data')
+
 let win
 
 function createWindow () {
-  win = new BrowserWindow({ width: 900, height: 500, titleBarStyle: 'hidden', show: false })
-  win.loadURL(`file://${__dirname}/production/index.html`)
-  win.on('closed', () => {
-    win = null
+  const storeWindow = new Store({
+    filename: 'window',
+    defaults: {
+      bounds: { x: '', y: '', width: 900, height: 500 }
+    }
   })
+  const { x, y, width, height } = storeWindow.get('bounds')
+
+  win = new BrowserWindow({ x: x, y: y, width: width, height: height, titleBarStyle: 'hidden', show: false })
+
+  win.loadURL('file://' + path.join(__dirname, 'production', 'index.html'))
+
   win.once('ready-to-show', () => {
     win.webContents.executeJavaScript('var webFrame = require("electron").webFrame; webFrame.setZoomLevelLimits(1, 1);')
     win.show()
+  })
+
+  win.on('move', () => {
+    storeWindow.set('bounds', win.getBounds())
+  })
+
+  win.on('resize', () => {
+    storeWindow.set('bounds', win.getBounds())
+  })
+
+  win.on('closed', () => {
+    win = null
   })
 }
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', () => {
-  app.quit()
-})
-
 app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
+})
+
+app.on('window-all-closed', () => {
+  app.quit()
 })
